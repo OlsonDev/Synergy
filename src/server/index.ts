a4984a618976483c3b5a9db35d23959935f283f5
Josh Olson <Olson.dev@Gmail.com>
2016-03-24 11:13:06 -0500

    Server now syncs src HTML to dist, reloads browser

--- src/server/index.ts
+++ src/server/index.ts
@@ -12,4 +12,29 @@ const options = {
 		}]
 	}
 };
-require('browser-sync').create().init(options);
+
+const bs = require('browser-sync').create();
+const fs = require('fs');
+
+function copyFile(source: string, target:string, cb: any) {
+	var cbCalled = false;
+	function done(err?: any) {
+		if (cbCalled) return;
+		cb(err);
+		cbCalled = true;
+	}
+
+	const rs = fs.createReadStream(source);
+	rs.on("error", function(err: any) { done(err); });
+	const ws = fs.createWriteStream(target);
+	ws.on("error", function(err: any) { done(err); });
+	ws.on("close", function(err: any) { done(); });
+	rs.pipe(ws);
+}
+
+bs.watch('src/browser/**/*.html').on('change', (path: string) => {
+	const dest = path.replace(/^src/, 'dist');
+	copyFile(path, dest, () => bs.reload('*.html'))
+});
+
+bs.init(options);
