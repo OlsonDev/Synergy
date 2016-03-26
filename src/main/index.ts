08050a5b825a2695970657d1521b7f8d9eeab630
Josh Olson <Olson.dev@Gmail.com>
2016-03-26 09:10:29 -0500

    Open window only after server is ready

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,16 +1,19 @@
 'use strict';
+const electron = require('electron');
+const http = require('http');
+const urlUtil = require('url');
 
 class Main {
-	constructor() {
-		const electron = require('electron');
+	mainWindow: Electron.BrowserWindow;
 
-		electron.app.on('window-all-closed', function() {
+	constructor() {
+		electron.app.on('window-all-closed', () => {
 			if (process.platform == 'darwin') return;
 			electron.app.quit();
 		});
 
-		electron.app.on('ready', function() {
-			let mainWindow = new electron.BrowserWindow({
+		electron.app.on('ready', () => {
+			this.mainWindow = new electron.BrowserWindow({
 				title: 'Synergy'
 				, width: 1920
 				, height: 1080
@@ -18,13 +21,35 @@ class Main {
 				, minHeight: 720
 				, backgroundColor: '#000'
 				, frame: false
+				, show: false
 			});
-
-			mainWindow.loadURL('http://192.168.1.2:3000/views/index.html');
-			mainWindow.webContents.openDevTools();
-
-			mainWindow.on('closed', (): any => mainWindow = null);
+			this.mainWindow.on('closed', (): any => this.mainWindow = null);
+			this.openWindowWhenServerReady('http://192.168.1.2:3000/views/index.html');
 		});
+	}
+
+	openWindowWhenServerReady(url: string) {
+		const opts = urlUtil.parse(url);
+
+		http.get(opts, (res: any) => {
+			if (res.statusCode == 200) {
+				this.openWindow(url);
+			} else {
+				setTimeout(() => {
+					this.openWindowWhenServerReady(url);
+				}, 50);
+			}
+		}).on('error', (err: any) => {
+			setTimeout(() => {
+				this.openWindowWhenServerReady(url);
+			}, 50);
+		});
+	}
+
+	openWindow(url: string) {
+		this.mainWindow.loadURL(url);
+		this.mainWindow.show();
+		this.mainWindow.webContents.openDevTools();
 	}
 }
 
