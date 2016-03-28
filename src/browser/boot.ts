f3c989ed8d706729b1e8ae1103d3827cc8a0a756
Josh Olson <Olson.dev@Gmail.com>
2016-03-28 17:31:16 -0500

    Create FPS counter component

--- src/browser/boot.ts
+++ src/browser/boot.ts
@@ -5,30 +5,27 @@ console.log('booting');
 import { Cleanup } from './components/misc/cleanup';
 import { Titlebar } from './components/misc/titlebar';
 import { DevTools } from './components/misc/dev-tools';
-import { Board } from './components/board';
 
 Cleanup.setup();
 Titlebar.setup();
 DevTools.setup();
 
 import * as PIXI from "pixi.js";
-
+import { Board } from './components/board';
+import { FpsCounter } from './components/fps-counter';
 
 const game = document.getElementById('game') as HTMLDivElement;
 const renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight, {
 	backgroundColor: 0x6495ED
 });
 game.appendChild(renderer.view);
+
 const stage = new PIXI.Container();
-
 const board = new Board();
+const fpsCounter = new FpsCounter();
 stage.addChild(board);
+stage.addChild(fpsCounter);
 
-animate();
-
-function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(stage);
-}
+PIXI.ticker.shared.add((time) => renderer.render(stage));
 
 console.log('booted');
---
+++ src/browser/components/fps-counter.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+export class FpsCounter extends PIXI.Text {
+	constructor() {
+		super('', {
+			fill: '#0f0'
+			, stroke: '#000'
+			, strokeThickness: 4
+			, font: 'bold 20px Arial'
+		});
+		this.anchor.x = 1;
+		this.anchor.y = 1;
+		this.position.x = window.innerWidth;
+		this.position.y = window.innerHeight;
+
+		this.on('added', () => {
+			console.log(this.parent.width, this.parent.height);
+		});
+
+		PIXI.ticker.shared.add((time) => {
+			this.text = Math.round(PIXI.ticker.shared.FPS).toString();
+		});
+	}
+}
